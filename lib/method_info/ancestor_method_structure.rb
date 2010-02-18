713b19065bb4acf868c6cb1913ffdbd495a4a27e
Tom ten Thij <git@tomtenthij.nl>
2010-02-18 16:46:17 +0000

    Update rdoc options for ancestor_method_structure

--- lib/method_info/ancestor_method_structure.rb
+++ lib/method_info/ancestor_method_structure.rb
@@ -1,15 +1,16 @@
 module MethodInfo
   class AncestorMethodStructure
-    # :ancestors_to_show (default: []) (Overrules the hiding of any ancestors as specified by the :ancestors_to_exclude option)
-    # :ancestors_to_exclude (default: []) (If a class is excluded, all modules included under it are excluded as well, an ancestor specified in :ancestors_to_show will be shown regardless of the this value)
-    # :method (default: nil) More detailed info about one method
+    # :ancestors_to_show (default: []) (Overrules the hiding of any ancestors as specified
+    #                    by the :ancestors_to_exclude option)
+    # :ancestors_to_exclude (default: []) (If a class is excluded, all modules included
+    #                       under it are excluded as well, an ancestor specified in
+    #                       :ancestors_to_show will be shown regardless of the this value)
     # :method_missing (default: false)
     # :public_methods (default: true)
     # :protected_methods (default: false)
     # :private_methods (default: false)
     # :singleton_methods (default: true)
     # :include_name_of_excluded_ancestors (default: true)
-    # :format (default: nil)
     def self.build(object, options)
       methods = []
       methods += object.methods if options[:public_methods]
