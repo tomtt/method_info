96fdf54baef1dd6a392a70a48169b0dbf4b0a90a
Tom ten Thij <git@tomtenthij.nl>
2010-04-28 01:46:02 +0100

    AncestorMethodMapping is working for versions of Ruby that don't provide the Method#owner method

--- lib/method_info/ancestor_method_mapping.rb
+++ lib/method_info/ancestor_method_mapping.rb
@@ -4,8 +4,8 @@ module MethodInfo
   class AncestorMethodMapping < Hash
     def initialize(object)
       @object = object
-      find_owner_for_each_method
       build_ancestor_hierarchy
+      find_owner_for_each_method
     end
 
     attr_reader :ancestors
@@ -14,9 +14,12 @@ module MethodInfo
 
     def build_ancestor_hierarchy
       @ancestors = @object.class.ancestors
-      @eigenclass = class << @object;self;end
-      unless @ancestors.include?(@eigenclass)
-        @ancestors = [@eigenclass] + @ancestors
+      begin
+        @eigenclass = class << @object;self;end
+        unless @ancestors.include?(@eigenclass)
+          @ancestors = [@eigenclass] + @ancestors
+        end
+      rescue TypeError
       end
     end
 
@@ -33,6 +36,39 @@ module MethodInfo
       method = Object.instance_method(:method).bind(@object).call(method_symbol)
 
       method.owner
+    rescue NoMethodError
+      poor_mans_method_owner(method, method_symbol.to_s)
+    end
+
+    # Ruby 1.8.6 has no Method#owner method, this is a poor man's replacement. It has horrible
+    # performance and may break for other ruby implementations than MRI.
+    def poor_mans_method_owner(method, method_name)
+      # A Method object has no :owner method, but we can infer it's owner from the result of it's
+      # :to_s method. Examples:
+      # 37.method(:rdiv).to_s => "#<Method: Fixnum#rdiv>"
+      # 37.method(:ceil).to_s => "#<Method: Fixnum(Integer)#ceil>"
+      # 37.method(:prec).to_s => "#<Method: Fixnum(Precision)#prec>"
+      # obj.method(:singleton_method).to_s => "#<Method: #<Object:0x5673b8>.singleton_method>"
+      # For a nested module: "#<Method: Module1::ClassName(Module1::Module2::Module3)#method>"
+
+      build_ancestor_regexp_map
+      @ancestors.each do |ancestor|
+        return ancestor if method.to_s =~ @ancestor_regexp_map[ancestor]
+      end
+      nil
+    end
+
+    def build_ancestor_regexp_map
+      unless @ancestor_regexp_map
+        @ancestor_regexp_map = Hash.new
+        ancestors.each do |ancestor|
+          ancestor_name = ancestor.to_s
+          if ancestor_name =~ /^#<Class:(.*)>$/
+            ancestor_name = $1
+          end
+          @ancestor_regexp_map[ancestor] = /#{Regexp.escape(ancestor_name)}[)#.]/
+        end
+      end
     end
   end
 end
--- spec/method_info/ancestor_method_mapping_spec.rb
+++ spec/method_info/ancestor_method_mapping_spec.rb
@@ -50,8 +50,102 @@ module MethodInfo
 
       it "should still find the owner of a method of an object that defines the method method" do
         object = Object.new
-        def object.method; end
+        def object.method
+          raise "I do not want to call this method"
+        end
         AncestorMethodMapping.new(object)[:respond_to?].should == Kernel
+      end
+
+      describe "for ruby versions not supporting Method#owner" do
+        it "should use the poor_mans_method_owner" do
+          # This is very implementation specific trickery to check that the owner of the method
+          # is found using the private poor_mans_method_owner method.
+          mock_method = mock('method', :to_s => '#<Method: String#foo>')
+          mock_method.stub!(:bind).and_return mock_method
+          mock_method.stub!(:call).and_return mock_method
+          mock_method.stub!(:owner).and_raise NoMethodError
+
+          Object.stub!(:instance_method).with(:method).and_return mock_method
+
+          object = "a string"
+          object.stub!(:methods).and_return([:foo])
+
+          mapping = AncestorMethodMapping.new(object)
+          mapping[:foo].should == String
+        end
+
+        it "raises an error if an error is raised that is not a NameError" do
+          mock_method = mock('method', :to_s => '#<Method: String#foo>')
+          mock_method.stub!(:bind).and_return mock_method
+          mock_method.stub!(:call).and_return mock_method
+          mock_method.stub!(:owner).and_raise StandardError
+
+          Object.stub!(:instance_method).with(:method).and_return mock_method
+
+          object = "a string"
+          object.stub!(:methods).and_return([:foo])
+
+          lambda { AncestorMethodMapping.new(object) }.should raise_error(StandardError)
+        end
+
+        describe "poor_mans_method_owner" do
+          it "finds the owner if it is the base clas" do
+            mapping = AncestorMethodMapping.new(37)
+            mapping.send(:poor_mans_method_owner, 37.method(:abs), "abs").should == Fixnum
+          end
+
+          it "finds the owner if it is a super clas" do
+            mapping = AncestorMethodMapping.new(37)
+            mapping.send(:poor_mans_method_owner, 37.method(:ceil), "ceil").should == Integer
+          end
+
+          it "finds the owner if it is a module" do
+            mapping = AncestorMethodMapping.new(37)
+            mapping.send(:poor_mans_method_owner, 37.method(:is_a?), "is_a?").should == Kernel
+          end
+
+          it "finds the owner if it is the eigenclass" do
+            obj = Object.new
+            def obj.foo
+              :foo
+            end
+            mapping = AncestorMethodMapping.new(obj)
+            eigenclass = class << obj; self; end
+            mapping.send(:poor_mans_method_owner, obj.method(:foo), "foo").should == eigenclass
+          end
+
+          it "finds the owner if it is nested" do
+            pending "Spec needs to be fixed to work with 1.9.1" if RUBY_VERSION >= "1.9.1"
+            module TestNestOne
+              module TestNestTwo
+                def nest
+                  :nest
+                end
+              end
+            end
+
+            class TestNest
+              include TestNestOne::TestNestTwo
+            end
+            obj = TestNest.new
+
+            mapping = AncestorMethodMapping.new(obj)
+            mapping.send(:poor_mans_method_owner, obj.method(:nest), "nest").should ==
+              MethodInfo::TestNestOne::TestNestTwo
+          end
+
+          it "finds the owner if it is an anonymous module" do
+            anon = Module.new
+            anon.send(:define_method, :bla) { :bla }
+            class UsingAnonymous
+            end
+            UsingAnonymous.send(:include, anon)
+            obj = UsingAnonymous.new
+
+            mapping = AncestorMethodMapping.new(obj)
+            mapping.send(:poor_mans_method_owner, obj.method(:bla), "bla").should == anon
+          end
+        end
       end
     end
 
@@ -92,6 +186,10 @@ module MethodInfo
         # nil's class and eigenclass are both NilClass
         subject.ancestors.should == subject.ancestors.uniq
       end
+    end
+
+    it "should not blow up when finding the ancestor hierarchy of an object with no eigenclass" do
+      lambda { AncestorMethodMapping.new(5) }.should_not raise_error
     end
   end
 end
