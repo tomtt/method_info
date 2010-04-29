123923e6dc29203f24d384f560124a34395928c4
Tom ten Thij <git@tomtenthij.nl>
2010-04-29 01:46:01 +0100

    Ability to specify which methods should be put in an ancestor_method_structure

--- lib/method_info/ancestor_method_structure.rb
+++ lib/method_info/ancestor_method_structure.rb
@@ -18,8 +18,9 @@ module MethodInfo
   # * Any method that does not have an owner or that has an owner that is not in the
   #   ancestors list will end up in the list with the nil ancestor
   class AncestorMethodStructure
-    def initialize(ancestor_method_mapping)
+    def initialize(ancestor_method_mapping, methods = nil)
       @ancestor_method_mapping = ancestor_method_mapping
+      @methods = methods || @ancestor_method_mapping.keys
       build_structure
     end
 
@@ -30,7 +31,7 @@ module MethodInfo
     def build_structure
       methods_by_owner = Hash.new { |hash, key| hash[key] = [] }
       ancestors = @ancestor_method_mapping.ancestors
-      @ancestor_method_mapping.keys.each do |method|
+      @methods.each do |method|
         ancestor = @ancestor_method_mapping[method]
         unless ancestors.include?(ancestor)
           ancestor = nil
--- spec/method_info/ancestor_method_structure_spec.rb
+++ spec/method_info/ancestor_method_structure_spec.rb
@@ -4,52 +4,73 @@ require 'method_info/ancestor_method_structure'
 module MethodInfo
   describe AncestorMethodStructure do
     describe "#structure" do
-      it "transforms an object that behaves like an AncestorMethodMapping into the desired structure" do
-        amm = {
-          :ape => Kernel,
-          :bear => Object,
-          :cat => Kernel,
-          :dog => String
-        }
-        amm.stub!(:ancestors).and_return [String, Object, Kernel]
-        AncestorMethodStructure.new(amm).structure.should ==
-          [[String, [:dog]],
-           [Object, [:bear]],
-           [Kernel, [:ape, :cat]]
-          ]
+      context "without methods passed" do
+        it "transforms an object that behaves like an AncestorMethodMapping into the desired structure" do
+          amm = {
+            :ape => Kernel,
+            :bear => Object,
+            :cat => Kernel,
+            :dog => String
+          }
+          amm.stub!(:ancestors).and_return [String, Object, Kernel]
+          AncestorMethodStructure.new(amm).structure.should ==
+            [[String, [:dog]],
+             [Object, [:bear]],
+             [Kernel, [:ape, :cat]]
+            ]
+        end
+
+        it "orders the methods alphabetically" do
+          amm = {
+            :gnu => Object,
+            :gnome => Object,
+            :goose => Object,
+            :geek => Object
+          }
+          amm.stub!(:ancestors).and_return [Object]
+          AncestorMethodStructure.new(amm).structure.should ==
+            [[Object, [:geek, :gnome, :gnu, :goose]]]
+        end
+
+        it "returns a structure with empty lists for ancestors without methods" do
+          amm = {}
+          amm.stub!(:ancestors).and_return [Fixnum, Object]
+          AncestorMethodStructure.new(amm).structure.should ==
+            [[Fixnum, []],
+             [Object, []]]
+        end
+
+        it "returns a structure with a 'nil' ancestors for methods that do not have an owner that is an ancestor" do
+          amm = {
+            :ape => Integer,
+            :bear => nil,
+            :cat => Fixnum
+          }
+          amm.stub!(:ancestors).and_return [Object]
+          AncestorMethodStructure.new(amm).structure.should ==
+            [[Object, []],
+             [nil, [:ape, :bear, :cat]]
+            ]
+        end
       end
 
-      it "orders the methods alphabetically" do
-        amm = {
-          :gnu => Object,
-          :gnome => Object,
-          :goose => Object,
-          :geek => Object
-        }
-        amm.stub!(:ancestors).and_return [Object]
-        AncestorMethodStructure.new(amm).structure.should ==
-          [[Object, [:geek, :gnome, :gnu, :goose]]]
-      end
-
-      it "returns a structure with empty lists for ancestors without methods" do
-        amm = {}
-        amm.stub!(:ancestors).and_return [Fixnum, Object]
-        AncestorMethodStructure.new(amm).structure.should ==
-          [[Fixnum, []],
-           [Object, []]]
-      end
-
-      it "returns a structure with a 'nil' ancestors for methods that do not have an owner that is an ancestor" do
-        amm = {
-          :ape => Integer,
-          :bear => nil,
-          :cat => Fixnum
-        }
-        amm.stub!(:ancestors).and_return [Object]
-        AncestorMethodStructure.new(amm).structure.should ==
-          [[Object, []],
-           [nil, [:ape, :bear, :cat]]
-          ]
+      context "with methods passed" do
+        it "transforms an object that behaves like an AncestorMethodMapping into the desired structure, using only the methods specified" do
+          amm = {
+            :ape => Kernel,
+            :bear => Object,
+            :cat => Kernel,
+            :dog => String,
+            :elk => Kernel,
+            :flamingo => Object
+          }
+          amm.stub!(:ancestors).and_return [String, Object, Kernel]
+          AncestorMethodStructure.new(amm, [:ape, :cat, :flamingo]).structure.should ==
+            [[String, []],
+             [Object, [:flamingo]],
+             [Kernel, [:ape, :cat]]
+            ]
+        end
       end
     end
   end
