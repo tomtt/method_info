37a37cd6e394a230bc578ad56e8e88c834a0d145
Tom ten Thij <git@tomtenthij.nl>
2010-05-09 02:55:47 +0100

    Simple output that matches cucumber feature

--- features/method_info.feature
+++ features/method_info.feature
@@ -11,9 +11,9 @@ Feature: Generating string representation of methods on an object
     Then I should see:
     """
 ::: Fixnum :::
-%, &, *, **, +, -, -@, /, <, <<, <=, <=>, ==, >, >=, >>, [], ^, abs, div, divmod, id2name, modulo, power!, quo, rdiv, rpower, size, to_f, to_s, to_sym, zero?, |, ~
+%, &, *, **, +, -, -@, /, <, <<, <=, <=>, ==, >, >=, >>, [], ^, abs, div, divmod, even?, fdiv, id2name, modulo, odd?, power!, quo, rdiv, rpower, size, to_f, to_s, to_sym, zero?, |, ~
 ::: Integer :::
-ceil, chr, denominator, downto, floor, gcd, gcdlcm, integer?, lcm, next, numerator, round, succ, taguri, taguri=, times, to_i, to_int, to_r, to_yaml, truncate, upto
+ceil, chr, denominator, downto, floor, gcd, gcdlcm, integer?, lcm, next, numerator, ord, pred, round, succ, times, to_i, to_int, to_r, truncate, upto
 ::: Precision :::
 prec, prec_f, prec_i
 ::: Numeric :::
@@ -21,9 +21,9 @@ prec, prec_f, prec_i
 ::: Comparable :::
 between?
 ::: Object :::
-to_yaml_properties, to_yaml_style
+
 ::: MethodInfo::ObjectMethod :::
 method_info
 ::: Kernel :::
-===, =~, __id__, __send__, class, clone, display, dup, equal?, extend, freeze, frozen?, hash, id, inspect, instance_eval, instance_of?, instance_variable_defined?, instance_variable_get, instance_variable_set, instance_variables, is_a?, kind_of?, method, methods, nil?, object_id, private_methods, protected_methods, public_methods, respond_to?, send, singleton_methods, taint, tainted?, to_a, type, untaint
+===, =~, __id__, __send__, class, clone, display, dup, enum_for, equal?, extend, freeze, frozen?, hash, id, inspect, instance_eval, instance_exec, instance_of?, instance_variable_defined?, instance_variable_get, instance_variable_set, instance_variables, is_a?, kind_of?, method, methods, nil?, object_id, private_methods, protected_methods, public_methods, respond_to?, send, singleton_methods, taint, tainted?, tap, to_a, to_enum, type, untaint
     """
--- lib/method_info/formatter.rb
+++ lib/method_info/formatter.rb
@@ -1,37 +1,34 @@
+require 'method_info/ancestor_method_mapping'
+require 'method_info/ancestor_method_structure'
+
 module MethodInfo
   # Can produce different formats to represent an AncestorMethodStructure
   class Formatter
     def self.build(object, options)
-      sample = <<EOT
-::: Fixnum :::
-%, &, *, **, +, -, -@, /, <, <<, <=, <=>, ==, >, >=, >>, [], ^, abs, div, divmod, id2name, modulo, power!, quo, rdiv, rpower, size, to_f, to_s, to_sym, zero?, |, ~
-::: Integer :::
-ceil, chr, denominator, downto, floor, gcd, gcdlcm, integer?, lcm, next, numerator, round, succ, taguri, taguri=, times, to_i, to_int, to_r, to_yaml, truncate, upto
-::: Precision :::
-prec, prec_f, prec_i
-::: Numeric :::
-+@, coerce, eql?, nonzero?, remainder, singleton_method_added, step
-::: Comparable :::
-between?
-::: Object :::
-to_yaml_properties, to_yaml_style
-::: MethodInfo::ObjectMethod :::
-method_info
-::: Kernel :::
-===, =~, __id__, __send__, class, clone, display, dup, equal?, extend, freeze, frozen?, hash, id, inspect, instance_eval, instance_of?, instance_variable_defined?, instance_variable_get, instance_variable_set, instance_variables, is_a?, kind_of?, method, methods, nil?, object_id, private_methods, protected_methods, public_methods, respond_to?, send, singleton_methods, taint, tainted?, to_a, type, untaint
-EOT
+      puts new(object, options).to_s
+    end
 
-      puts sample
-      # new(object, options)
+    def to_s
+      ss = @ancestor_method_structure.structure.map do |ancestor_with_methods|
+        ancestor_with_methods_to_s(ancestor_with_methods)
+      end
+      ss.join("")
+    end
+
+    private
+
+    def ancestor_with_methods_to_s(ancestor_with_methods)
+      (ancestor, methods) = ancestor_with_methods
+      "::: %s :::\n" % ancestor + methods.join(", ") + "\n"
     end
 
     def initialize(object, options)
-      # @object = object
-      # @options = options
-      # @ancestor_method_mapping = AncestorMethodMapping.new(object)
+      @object = object
+      @ancestor_method_mapping = AncestorMethodMapping.new(object)
+      @methods = @object.methods
       # select_methods
       # apply_match_filter_to_methods
-      # @ancestor_method_structure = AncestorMethodStructure.new(@ancestor_method_mapping, @methods)
+      @ancestor_method_structure = AncestorMethodStructure.new(@ancestor_method_mapping, @methods)
       # @ancestor_filter = AncestorFilter.new(@ancestor_method_mapping.ancestors,
       #                                       :include => options[:ancestors_to_show],
       #                                       :exclude => options[:ancestors_to_exclude])
